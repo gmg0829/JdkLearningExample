9ff53cebb4ff34fc35982d74eef3b8d8ecd17b30
gmg <minggang0829@gmail.com>
2018-09-17 17:40:07 +0800

    模板方法模式

---
+++ src/main/java/com/gmg/template/AbstractSort.java
@@ -0,0 +1,20 @@
+package com.gmg.template;
+
+/**
+ * @author gmg
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2018/9/17  17:35
+ */
+public abstract class AbstractSort {
+    protected abstract void sort(int[] arrray);
+
+    public void showSortResult(int[] array){
+        this.sort(array);
+        System.out.print("排序结果：");
+        for (int i = 0; i < array.length; i++){
+            System.out.printf("%3s", array[i]);
+        }
+    }
+}
---
+++ src/main/java/com/gmg/template/Client.java
@@ -0,0 +1,16 @@
+package com.gmg.template;
+
+/**
+ * @author gmg
+ * @Title:
+ * @Package
+ * @Description: 模板方法模式
+ * @date 2018/9/17  17:37
+ */
+public class Client {
+    public static int[] a = { 10, 32, 1, 9, 5, 7, 12, 0, 4, 3 };
+    public static void main(String[] args) {
+        AbstractSort abstractSort=new ConcreteSort();
+        abstractSort.showSortResult(a);
+    }
+}
