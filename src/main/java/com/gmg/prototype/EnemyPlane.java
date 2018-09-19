bf87f57415dedeab81e3067946f10e8b7a757dba
gmg <minggang0829@gmail.com>
2018-09-19 16:50:43 +0800

    原型模式

---
+++ src/main/java/com/gmg/prototype/EnemyPlane.java
@@ -0,0 +1,46 @@
+package com.gmg.prototype;
+
+/**
+ * @author gmg
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2018/9/19  16:24
+ */
+public class EnemyPlane implements Cloneable {
+    private int x;//敌机横坐标
+    private int y = 0;//敌机纵坐标
+
+    public EnemyPlane(int x) {//构造器
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void fly(){//让敌机飞
+        y++;//每调用一次，敌机飞行时纵坐标＋1
+    }
+
+    //此处开放setX，为了让克隆后的实例重新修改x坐标
+    public void setX(int x) {
+        this.x = x;
+    }
+    //为了保证飞机飞行的连贯性
+    //这里我们关闭setY方法，不支持随意更改Y纵坐标
+//    public void setY(int y) {
+//        this.y = y;
+//    }
+
+
+    @Override
+    protected EnemyPlane clone() throws CloneNotSupportedException {
+        EnemyPlane clonePlane = (EnemyPlane) super.clone();
+        return clonePlane;
+    }
+}
