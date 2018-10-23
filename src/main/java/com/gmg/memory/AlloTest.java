f74c210997b35b6695b04e43fa8aa78a3df15c4b
gmg <minggang0829@gmail.com>
2018-10-23 10:24:35 +0800

    commit

---
+++ src/main/java/com/gmg/memory/AlloTest.java
@@ -0,0 +1,47 @@
+package com.gmg.memory;
+
+/**
+ * @author gmg
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2018/10/23  10:03
+ */
+public class AlloTest {
+    private static final int _1MB = 1024 * 1024;
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     */
+    public static void testAllocation() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation4 = new byte[4 * _1MB];  // 出现一次Minor GC
+    }
+    //大对象  -XX:PretenureSizeThreshold=3145728
+    public static void testPretenureSizeThreshold() {
+        byte[] allocation;
+        allocation = new byte[4 * _1MB];  //直接分配在老年代中
+    }
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1
+     * -XX:+PrintTenuringDistribution
+     */
+    @SuppressWarnings("unused")
+    public static void testTenuringThreshold() {
+        byte[] allocation1, allocation2, allocation3;
+        allocation1 = new byte[_1MB / 4];  // 什么时候进入老年代决定于XX:MaxTenuringThreshold设置
+        allocation2 = new byte[4 * _1MB];
+        allocation3 = new byte[4 * _1MB];
+        allocation3 = null;
+        allocation3 = new byte[4 * _1MB];
+    }
+
+    public static void main(String[] args) {
+        testTenuringThreshold();
+    }
+}
+
