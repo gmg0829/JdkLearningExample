3c33252b270bab8b550a646398fb645d69cf5d3f
gmg0829 <minggang0829@gmail.com>
2019-03-28 17:15:15 +0800

    commit

---
+++ src/main/java/com/gmg/stream/StreamReduce.java
@@ -0,0 +1,26 @@
+package com.gmg.stream;
+
+import java.util.stream.Stream;
+
+/**
+ * @author gmg
+ * @title: StreamReduce
+ * @projectName JdkLearningExample
+ * @description: TODO
+ * @date 2019/3/28 17:10
+ */
+public class StreamReduce {
+    public static void main(String[] args) {
+        String concat = Stream.of("A", "B", "C", "D").reduce("", String::concat);
+        // 求最小值，minValue = -3.0
+        double minValue = Stream.of(-1.5, 1.0, -3.0, -2.0).reduce(Double.MAX_VALUE, Double::min);
+        // 求和，sumValue = 10, 有起始值
+        int sumValue = Stream.of(1, 2, 3, 4).reduce(0, Integer::sum);
+        // 求和，sumValue = 10, 无起始值
+        sumValue = Stream.of(1, 2, 3, 4).reduce(Integer::sum).get();
+        // 过滤，字符串连接，concat = "ace"
+        concat = Stream.of("a", "B", "c", "D", "e", "F").
+                filter(x -> x.compareTo("Z") > 0).
+                reduce("", String::concat);
+    }
+}
