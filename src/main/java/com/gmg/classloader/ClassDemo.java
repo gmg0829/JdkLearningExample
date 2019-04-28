ed89ab3e17507370c08455ce514d597e94460d2d
gmg0829 <minggang0829@gmail.com>
2019-04-28 10:46:20 +0800

    commit

--- src/main/java/com/gmg/classloader/ClassDemo.java
+++ src/main/java/com/gmg/classloader/ClassDemo.java
@@ -1,5 +1,9 @@
 package com.gmg.classloader;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.security.cert.Extension;
+
 /**
  * @author gmg
  * @title: ClassDemo
@@ -8,11 +12,24 @@ package com.gmg.classloader;
  * @date 2019/4/28 10:16
  */
 public class ClassDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
       ClassLoader classLoader=ClassDemo.class.getClassLoader();
       while (classLoader!=null){
           System.out.println(classLoader.toString());
           classLoader=classLoader.getParent();
       }
+       // URLClassLoader
+//        ClassLoader
+        Thread.currentThread().getContextClassLoader().loadClass("");
+        ClassLoader.getSystemClassLoader().loadClass("");
+
+
+        String v1dir = "file:///Users/qianwp/source/jcl/v1/";
+        URLClassLoader v1 = new URLClassLoader(new URL[]{new URL(v1dir)});
+
+        Class<?> depv1Class = v1.loadClass("Dep");
+        Object depv1 = depv1Class.getConstructor().newInstance();
+        depv1Class.getMethod("print").invoke(depv1);
+
     }
 }
